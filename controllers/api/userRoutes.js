78b8507f001ec59d3a4a4ae37ee76f8d83d550e0
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-06-07 22:03:08 -0500

    userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+const { User } = require('../../models')
+
+router.post('/signup', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = newUser.isSoftDeleted;
+            req.session.logged_in = true;
+            res.status(200).json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { username: req.body.username }})
+        if(!user) {
+            res.status(400).json({ message: 'Incorrect username or password entered.'})
+            return;
+        }
+
+        const validPassword = await user.checkPassword(req.body.password)
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password entered.'})
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            req.session.logged_in = true;
+            res.json({ user, message: 'You are logged in!' })
+        })
+    } catch (er) {
+        res.status(500).json(err)
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.redirect('/')
+        })
+    } else {
+        res.status(400).end()
+    }
+})
+
+module.exports = router;
