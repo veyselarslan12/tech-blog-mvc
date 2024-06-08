0180c27a5f1e0bd69bf2a87b71d6a8323f964770
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-06-07 23:44:22 -0500

    post routes - delete

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -35,5 +35,23 @@ router.put('/:id', withAuth, async (req, res) => {
 })
 
 router.delete('/:id', withAuth, async (req, res) => {
-    
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        })
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found'})
+            return
+        } 
+
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+module.exports = router
